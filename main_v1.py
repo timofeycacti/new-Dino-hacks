30400d6ebe6970f3a219df98d4e9e1c82aab7486
timofeycacti <149524909+timofeycacti@users.noreply.github.com>
2025-01-15 21:36:17 +0100

    Clouds & kill
    k=kill
    c=summon a cloud

---
+++ main_v1.py
@@ -0,0 +1,25 @@
+import subprocess
+import time
+import pychrome
+from pynput import keyboard
+import threading
+import random
+
+browser = pychrome.Browser(url="http://127.0.0.1:1234")
+print(__file__)
+tab = browser.new_tab("chrome://dino")
+tab.start()
+
+def on_press(key):
+    global tab
+    try:
+        if key.char == 'k':
+            tab.Runtime.evaluate(expression="Runner.instance_.gameOver()")
+        elif key.char =='c':
+            tab.Runtime.evaluate(expression="Runner.instance_.horizon.addCloud()")
+    except:
+        print("oshibka!")
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
+
